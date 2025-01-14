642c3e26359491d90bed91626d99688f8a527f2d
Arheee <justinewallet1@gmail.com>
2025-01-14 15:24:22 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+    public class TodoItemController : Controller
+    {
+        private readonly TodoContext _context;
+        public TodoItemController(TodoContext context)
+        {
+            _context = context;
+        }
+    
+        public ActionResult Index()
+        {
+            var todos = _context.Todos.ToList();
+
+            return View(todos);
+        }
+
+        [HttpGet] //formulaire
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost] //ajouter une tache 
+        public ActionResult Form(TodoItemModel oform)
+        {
+            return View(oform);
+        }
+
+      
+        //Supp une tache
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+        // Marquer une tâche comme terminée
+        public IActionResult ToggleComplete(int id)
+        {
+            var todo = _context.Todos.Find(id);
+            if (todo != null)
+            {
+                todo.IsCompleted = !todo.IsCompleted;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItemModel> Todos { get; set; }
+    }
+}
